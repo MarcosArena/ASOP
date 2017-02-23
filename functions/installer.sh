b6388f44bf18850acd3296a4ece265fa17193c41
OscarCobo <ocobo97@gmail.com>
2017-02-23 19:50:58 +0100

    Update installer.sh

--- functions/installer.sh
+++ functions/installer.sh
@@ -10,12 +10,12 @@ if [ "$USER" = "root" ];
         echo recordatorios >> /etc/cron.deny
 
         #Añadimos el script de inicio que comprobara si esta creada la estructura, si no lo esta la creara.
-        mv /home/user/checkStructure.sh /etc/init.d/
+        mv /recordatorios/functions/checkStructure.sh /etc/init.d/
         chmod +x /etc/init.d/checkStructure.sh
         sudo update-rc.d checkStructure.sh defaults
-
         #Añadimos el directorio donde se encontrara nuestro script a PATH para hacerlo accesible desde cualquier parte
-        PATH="$PATH:/home/user/recordatorios/ASOP-master/functions"
+        PATH="$PATH:/recordatorios/functions"
+	PATH="$PATH:/recordatorios"
 
 else
 echo "Se necesitan privilegios de administrador para realizar la instalación de esta herramienta"

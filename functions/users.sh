d073ce5e7ceccbfffc03a7b0f75646a1e93c539f
OscarCobo <ocobo97@gmail.com>
2017-02-23 18:56:31 +0100

    Update users.sh

--- functions/users.sh
+++ functions/users.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+#Oscar Cobo Gomez
 . ./functions/getRecordatorio.sh
 . ./functions/createRecordatorio.sh
 . ./functions/addToCron.sh
 function getUsers(){
 	string=$3
-	totalidad=`expr length $string`
-	while true
+	totalidad=`expr length $string` #Guarda en una variable la longitud de la cadena introducida
+	while true #bucle infinito
 	do
-		coma=`expr index $string ","`
+		coma=`expr index $string ","` #Guarda la posicion de la primera ,
 		if [ $coma -eq 0 ]
 			then
 				VALID_ID=$(getFilename) #GenerarID
@@ -19,14 +20,14 @@ function getUsers(){
 		else
 			let coma1=$coma+1
 			let sincoma=$coma-1
-			usuarioActual=`expr substr $string 1 $sincoma`			
+			usuarioActual=`expr substr $string 1 $sincoma` #Encuentra un usuario			
 			VALID_ID=$(getFilename) #GenerarID
 			usuario=$usuarioActual
 			getRecordatorio  "$timeAndDate" "$1" "$2" $usuario
 			finalString="$timeAndDate $usuario bash $ruta_script/"$tituloWithUnder"_"$VALID_ID".sh"
 			addToCron
 			let totalidad=$totalidad-$coma
-			string=`expr substr $string $coma1 $totalidad`
+			string=`expr substr $string $coma1 $totalidad` #Modifica el string introducido para eliminar el usuario anterior
 		fi
 	done
 }

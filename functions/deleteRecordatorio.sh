ce59e05e1fd2f1c83d4946f13a9f2b2d9abca9aa
German <germangaldon96@gmail.com>
2017-02-24 19:57:11 +0100

    Add files via upload

--- functions/deleteRecordatorio.sh
+++ functions/deleteRecordatorio.sh
@@ -1,28 +1,31 @@
 
 #Comprobamos si es root, si es root eliminamos la linea del archivo contrab del root, si no lo es, eliminamos la linea del archivo de respaldo .cron.txt del usuario y ejecutamos crontab para actualizar el fichero.
-if [ $USER=="root" ];
+echo $USER
+if [ $USER="root" ];
         then
 	archive=`find ~/recordatorios/ -name "$2*".sh`
-	if [ ! -z "$2" ] && [ ! -z "$archive" ]; 	
+	if [ ! -z "$2" ] && [ ! -z "$archive" ];
 	then
-		sed -i '/$2/d' /etc/crontab
+		echo $USER
+		sed -i "/${2}/d" /etc/crontab
 		echo $archive
 		#Eliminamos el script de la carpeta recordatorios.
-		rm $archive
+		#rm $archive
 		echo "...eliminando tarea programada."
 		echo "...done."
 		echo "root"
-		else
+	else
 		echo "Error de formato, no ha introducido ninguna tarea para eliminar la tarea no existe."
 	fi
 else
 	if [ ! -z "$2" ] && [ ! -z "$archive" ]; 
 	then
-		sed -i '/$2/d' ~/recordatorios/.cron.txt
+		sed -i "/${2}/d" ~/recordatorios/.cron.txt
 		crontab ~/recordatorios/.cron.txt
-		rm $archive
+		#rm $archive
 		echo "...eliminando tarea programada."
 		echo "...done."
+		echo "user"
 	else
 		echo "Error de formato, no ha introducido ninguna tarea para eliminar la tarea no existe."
 	fi

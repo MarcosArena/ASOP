f228ba64c745706e396e70130eb58f619b8ed9d0
OscarCobo <ocobo97@gmail.com>
2017-02-25 13:30:13 +0100

    Add files via upload

---
+++ recordatorios.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+#Oscar Cobo Gomez
+. ./functions/sintaxis.sh
+. ./functions/addToCron.sh
+. ./functions/users.sh
+. ./functions/checkDate.sh
+. ./functions/checkTimeFormat.sh
+. ./functions/generateID.sh
+. ./functions/checkExists.sh
+. ./functions/getFileName.sh
+. ./functions/getRecordatorio.sh
+. ./functions/createRecordatorio.sh
+. ./functions/deleteRecordatorio.sh
+. ./functions/z_menu.sh
+case $1 in
+	-a)
+		if [ "$USER" = "root" ] #Si el usuario es Root entrará en el bucle
+			then
+				if [ $# -eq 6 ] #Si se han establecido 6 parametros es que ha indicado algun usuario
+					then 
+						checkDate $2 $3 #tratemiento fecha y hora
+						string "$4" #miramos que en el texto introducido no haya ';'
+						string "$5" #miramos que en el texto introducido no haya ';'
+						getUsers "$4" "$5" "$6" #empezar iteracion de usuarios y genera el recordatorio para cada uno de ellos
+				fi
+				if [ $# -eq 5 ] #Usuario root pero recordatorio para él mismo
+					then
+						checkDate $2 $3 #tratemiento fecha y hora
+						string "$4" #miramos que en el texto introducido no haya ';'
+						string "$5" #miramos que en el texto introducido no haya ';'
+						VALID_ID=$(getFilename) #GenerarID
+						usuario="root"
+						getRecordatorio  "$timeAndDate" "$4" "$5" $usuario
+						finalString="$timeAndDate $usuario bash $ruta_script/"$tituloWithUnder"_"$VALID_ID".sh"
+						addToCron
+				fi
+		else
+			if [ $# -eq 5 ]
+				then 
+					checkDate $2 $3 #tratemiento fecha y hora
+					string "$4" #miramos que en el texto introducido no haya ';'
+					string "$5" #miramos que en el texto introducido no haya ';'
+					VALID_ID=$(getFilename) #GenerarID
+					usuario=""
+					getRecordatorio  "$timeAndDate" "$4" "$5" $usuario
+					finalString="$timeAndDate bash $ruta_script/"$tituloWithUnder"_"$VALID_ID".sh"
+					addToCron	
+			else
+				echo "La sintaxis introducida no es correcta, pruebe a ejecutar recordatorios -h"
+			fi	
+		fi
+		;;
+	-d)
+		if [ $# -eq 2 ]
+			then
+				echo "listar recordatorios del usuario"
+				delete $2
+		else
+			echo "La sintaxis introducida no es correcta, pruebe a ejecutar recordatorios -h"
+		fi
+		;;
+	-m)
+		echo "modificar"
+		string "$5"
+		string "$6"
+		;;
+	-i)
+		echo "historico"
+		;;
+	-l)
+		echo "listar"
+		;;
+	-c)
+		echo "consultas"
+		;;
+	"")
+		z_menu
+		;;
+	-h)
+		echo ""
+		echo "RECORDATORIOS"
+		echo "es una orden para gestionar los recordatorios del usuario"
+		echo "si se ejecuta sin ningun parametro se ejecutará con entrono grafico"
+		echo "los parametros para utilizar son los siguientes:"
+		echo -e "     -a: Añade un nuevo recordatorio"
+		echo -e "                dd/mm/yyyy hh:mm \"Titulo Recordatorio\" \"Descripcion Recordatorio\""
+		echo -e "     -d: Elimina el recordatorio que contiene la ID introducida."
+		echo -e "                ID"
+		echo -e "     -m: Modifica un recordatorio existente."
+		echo -e "                sintaxis para modificar"
+		echo -e "     -i: Muestra un historico de los recordatorios del usuario"
+		echo -e "                [usuario]"
+		echo -e "     -l: listar"
+		echo -e "                sintaxis"
+		echo -e "     -c: consultar"
+		echo -e "                sintaxis"
+		;;
+	*)
+		echo "El parametro introducido no es correcto, pruebe a ejecutar recordatorios -h"
+		;;
+esac

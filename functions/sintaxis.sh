369eaf8cd73097a6aa03f14e0a8788b1c18c18f1
OscarCobo <ocobo97@gmail.com>
2017-02-25 16:07:51 +0100

    Update sintaxis.sh

--- functions/sintaxis.sh
+++ functions/sintaxis.sh
@@ -1,12 +1,12 @@
 #Oscar Cobo Gomez
 function string(){
-	contador=0
+	contador=0 #establecemos un contador para utilizar en el bucle
 	contador1=1
-	longitud=`expr length "$1"`
-	while [ $contador -lt $longitud ]
+	longitud=`expr length "$1"` #establecemos una variable llamada longitud que guarda la longitud del string introducido
+	while [ $contador -lt $longitud ] #bucle que hara itiraciones hasta que el contador previamente establecido sea igual a longitud
 	do
-		comprobar=`expr substr "$1" $contador1 1`
-		if [ "$comprobar" = ";" ]
+		comprobar=`expr substr "$1" $contador1 1` #Comprobamos si el caracter actual es un ';'
+		if [ "$comprobar" = ";" ] #Si se trata de un ';' se acabará el bucle y mostrara un error.
 			then
 				echo "No se puede introducir el caracter ';'"
 				exit

b47a070274c656ec306e3a6867f11bb945fbd8fc
OscarCobo <ocobo97@gmail.com>
2017-02-27 23:04:32 +0100

    Delete checkExists.sh

--- functions/checkExists.sh
+++
@@ -1,8 +0,0 @@
-# Funcion que comprueba si el parametro pasado (id) existe como nombre de archivo
-function checkExists {
-   if [ $(find ~/recordatorios/ -name *$1* ) ] ; then
-      echo 1
-   else
-      echo 0
-   fi
-}

028a6b3710a3e0416d17416977f0c9eb096e7a9e
OscarCobo <ocobo97@gmail.com>
2017-02-28 01:29:21 +0100

    Update z_menu.sh

--- functions/z_menu.sh
+++ functions/z_menu.sh
@@ -4,13 +4,12 @@
 . ./functions/z_add.sh
 . ./functions/z_delete.sh
 function z_menu(){
-list=`zenity --list --radiolist --print-column='3' --separator=',' --title="Seleccione un opción" --text="Elija una opción para empezar."\
+list=`zenity --list --radiolist --print-column='2' --separator=',' --title="Seleccione un opción" --text="Elija una opción para empezar."\
 	--width=400 --height=400 \
 	--column="" --column="Opción" --column="a"\
-    --hide-column='3' \
+    --hide-column='2' \
 	"" "Crear un recordatorio" "crear" \
-	"" "Borrar un recordatorio" "borrar" \
-    "" "Editar un recordatorio" "editar"
+	"" "Borrar un recordatorio" "borrar"
 	`
 
 case $list in 
@@ -18,16 +17,13 @@ case $list in
 		z_add;;	
 	"borrar") 	
 		z_delete;;
-	"editar")
-		#bash ./functions/z_edit.sh
-;;
-    "")
-        clear
-        zenity --question --title="Atención" --text="¿Seguro que deseas cancelar la operación?"	
+    	"")
+      		clear
+        	zenity --question --title="Atención" --text="¿Seguro que deseas cancelar la operación?"	
 			if [ $? = 0 ]	
 				then 
-                clear
-                exit
+                			clear
+               				exit
 			else
 				bash ./z_menu.sh
 			fi

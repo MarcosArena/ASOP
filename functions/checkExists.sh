a03b7c60e97871c2dbfc279f668b10ad4e8077d5
OscarCobo <ocobo97@gmail.com>
2017-02-28 20:11:52 +0100

    Update checkExists.sh

--- functions/checkExists.sh
+++ functions/checkExists.sh
@@ -1,4 +1,5 @@
 # Funcion que comprueba si el parametro pasado (id) existe como nombre de archivo
+#Autor: Diego
 function checkExists {
    if [ $(find ~/recordatorios/ -name *$1* ) ] ; then
       echo 1

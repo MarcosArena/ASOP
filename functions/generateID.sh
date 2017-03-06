dd16ba1ebd6479cdf16d570d8b55560697f0cfff
Diego <diego@diego.cat>
2017-03-06 21:03:11 +0100

    Added option to show pendent or finished tasks

--- functions/generateID.sh
+++
@@ -1,7 +0,0 @@
-#AUTOR -> DIEGO
-
-# Funcion que genera una cadena alfanumerica de 8 caracteres
-function generateID {
-   id=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-   echo $id
-}
--- functions/listAll.sh
+++ functions/listAll.sh
@@ -1,4 +1,9 @@
 #/bin/bash
+
+
+
+
+
 #
 # Grepfiles: busca los recordatorios almacenados en la ruta especificada
 # 
@@ -7,10 +12,16 @@
 #
 # Autor: Diego
 #
+
 function grepfiles {
 	ruta="$1/recordatorios/scripts/*.sh"
 	echo $ruta
 }
+
+
+
+
+
 
 
 #
@@ -22,19 +33,55 @@ function grepfiles {
 #
 # Autor: Diego
 #
+
 function printtable {
+
+	eleccion="x"
+	ahora=$(date +'%s')
+	while [[ $eleccion == "x" ]]
+	do
+		echo " Indique los recordatorios a listar"
+		echo -n " t: todos - p: pendientes - f: finalizados. Opcion: "
+		read eleccion
+
+		case $eleccion in
+			"t" ) eleccion="t";;
+			"p" ) eleccion="p";;
+			"f" ) eleccion="f";;
+			*) eleccion="x";;
+		esac
+	done
 
 	echo " "
 	echo " Lista de recordatorios de $USER"
 	echo " "
-	printf "%-3s %-30s %-60s\n" "" "Titulo" "Descripcion"
+	printf "%-3s %-20s %-30s %16s\n" "" "Titulo" "Descripcion" "Fecha"
 	echo " "
 
 	for r in $1
 	do
 		titulo=$(grep -Eho '^\#Titulo:\s(.*)' $r | cut -d ' ' -f 2-)
 		desc=$(grep -Eho '^\#Descripcion:\s(.*)' $r | cut -d ' ' -f 2-)
-		printf "%-3s %-30s %-60s\n" " *" "${titulo:0:30}" "${desc:0:60}"
+		fecha=$(grep -Eho '^\#Fecha:\s([0-9]+)' $r | cut -d ' ' -f 2-)
+		if [ -z $fecha ]; then
+			fecha=0
+		fi
+		print=0
+		
+		if [[ $eleccion == "t" ]]; then
+			print=1
+		elif [[ $eleccion == "p" ]] && [[ $fecha -gt $ahora ]]; then
+			print=1
+		elif [[ $eleccion == "f" ]] && [[ $fecha -lt $ahora ]]; then
+			print=1
+		else
+			print=0
+		fi
+
+		if [ $print == 1 ]; then
+			fechalegible=$(date -d @$fecha +'%H:%M %d/%m/%Y')
+			printf "%-3s %-20s %-30s %16s\n" " *" "${titulo:0:20}" "${desc:0:30}" "${fechalegible:0:16}"
+		fi
 	done
 
 	echo " "
@@ -42,35 +89,52 @@ function printtable {
 	echo " "
 }
 
-###Todo esto está comentado porque se introduzco en el parámetro -l de recordatorios.sh
 
-#if [ $UID == '0' ] 
-#then
-#	if [ -z $1 ]
-#	then
-#		echo "Debes introducir un nombre de usuario"
-#		exit
-#	fi
-#	archivos=$(grepfiles "/home/$1")	
-#	if [[ $archivos =~ \* ]]
-#	then
-#		echo "No se han encontrado recordatorios"
-#		exit
-#	else 
-#		printtable "$archivos"
-#	fi
-#else
-#	if [[ -n $* ]] && [[ $* -ne "$USER" ]]
-#	then
-#		echo "No puedes ver los recordatorios de otros usuarios"
-#		exit
-#	fi
-#	archivos=$(grepfiles ~)
-#	if [[ $archivos =~ \* ]]
-#	then
-#		echo "No se han encontrado recordatorios"
-#		exit
-#	else 
-#		printtable "$archivos"
-#	fi
-#fi
+
+
+
+
+if [ $UID == '0' ] 
+then
+
+
+
+	if [ -z $1 ]
+	then
+		echo "Debes introducir un nombre de usuario"
+		exit
+	fi
+	
+	archivos=$(grepfiles "/home/$1")
+	
+	if [[ $archivos =~ \* ]]
+	then
+		echo "No se han encontrado recordatorios"
+		exit
+	else 
+		printtable "$archivos"
+	fi
+
+else
+
+
+	if [[ -n $* ]] && [[ $* -ne "$USER" ]]
+	then
+		echo "No puedes ver los recordatorios de otros usuarios"
+		exit
+	fi
+
+	archivos=$(grepfiles ~)
+
+	if [[ $archivos =~ \* ]]
+	then
+		echo "No se han encontrado recordatorios"
+		exit
+	else 
+		printtable "$archivos"
+	fi
+
+
+fi
+
+

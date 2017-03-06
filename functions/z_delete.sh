a364ee97ed6e4f671487fec56d9b10133f855183
MarcosArena <caspcrew92@gmail.com>
2017-03-06 12:38:01 +0100

    Add files via upload

--- functions/z_delete.sh
+++ functions/z_delete.sh
@@ -3,6 +3,8 @@
 #AUTOR -> MARCOS ARENA
 function z_delete(){
 
+
+
 if [ "$USER" == "root" ]
     then
         #Si ha introducido un usuario como parametro -> ruta del home del usr
@@ -16,12 +18,21 @@ else
     ruta_script="$HOME/recordatorios"
 fi
 
+
+
 archivo=`zenity --file-selection --filename=$ruta_script/ --title="Selecciona un archivo"`
+
+arrID=(${archivo//_/ })
+ID=${arrID[1]}
+
+#Quitamos el .sh
+ID=`expr substr $ID 1 8`
+
+
 
 case $? in 
 	0) 
-		#delete $archivo
-		rm -- $archivo;;	
+		delete $ID;;
 	1) 	
 		echo "No ha seleccionado ningún archivo.";;
 	2)
